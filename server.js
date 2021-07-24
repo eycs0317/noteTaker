ef56e5b137b5e8a69cc059aa1ed8d8027dcba8ff
Eddie Yeung <eycs0317@gmail.com>
2021-07-23 21:26:35 -0700

    add * route

--- server.js
+++ server.js
@@ -60,6 +60,10 @@ app.post('/api/notes', (req, res) => {
     res.send('delete sucess!!')
   })
 
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+  })
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
